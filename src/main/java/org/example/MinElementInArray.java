9d31beb1671453a20d00edb02098fb140ca1cba8
dvcsr <4th3sh0ws@gmail.com>
2024-09-05 08:18:08 +0700

    refactor sec element code

--- src/main/java/org/example/MinElementInArray.java
+++ src/main/java/org/example/MinElementInArray.java
@@ -20,35 +20,12 @@ public class MinElementInArray {
         Optional<Integer> minValue =
                 numList.stream()
                         .reduce((n1, n2) -> n1 < n2? n1:n2);
-        System.out.println(minValue.get());
-        int num = minValue.get();
-
+        minValue.ifPresent(e -> System.out.println("The smallest number is: " + e));
 
         numList.sort(Comparator.naturalOrder());
         System.out.println("the second smallest number is: " + numList.get(1));
 
-
-
-
-
-
-        //minValue.ifPresent(integer -> numList.remove(integer));
-
-        //rnumList.remove(Integer.valueOf(remove));
-
-       // Optional<Integer> secondMinValue =
-              //  numList.stream()
-                   //     .reduce((n1, n2) -> n1 < n2? n1:n2);
-
-        //System.out.println(secondMinValue.get());
-        //secondMinValue.ifPresent(integer -> System.out.println("second min value is: " + integer));
-        //System.out.println("The least numbers is " + minValue.get());
         }
-    //public static int findSecondSmallest(List<Integer> list, int n1){
-     //   int result = 0;
-       // result = list.stream().sorted().reduce()
-       // return result;
-
     }
 
 
